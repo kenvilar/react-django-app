eb7f38117482937777f20305a9d563966571d6a5
Ken Vilar <kenvilar@gmail.com>
2020-10-12 20:00:25 +0800

    return the character count of the submitted text

--- backend/char_count/views.py
+++ backend/char_count/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def char_count(request):
+    text = request.GET.get("text", "")
+
+    return JsonResponse({"count": len(text)})
