fa32a4a17c36829928191bd66da25e3b52f597b9
Ken Vilar <kenvilar@gmail.com>
2020-10-12 20:56:28 +0800

    Implement frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <label htmlFor="char-input">How many characters does</label>
+        <input type="text" id='char-input'/>
+        <button onClick={handleSubmit}>have?</button>
+      </div>
+      <div>
+        <h3 id='char-count'></h3>
+      </div>
     </div>
   );
+}
+
+function handleSubmit(event) {
+  const text = document.querySelector('#char-input').value;
+
+  axios.get(`/char_count?text=${text}`)
+    .then(({data}) => {
+      document.querySelector(
+        '#char-count').textContent = `${data.count} characters!`;
+    })
+    .catch(err => console.log(err));
 }
 
 export default App;
