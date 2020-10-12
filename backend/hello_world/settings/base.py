48b63e5c976337cb0c493608820e508e41712e67
Ken Vilar <kenvilar@gmail.com>
2020-10-12 21:16:15 +0800

    Update settings for different environments

--- backend/hello_world/settings/base.py
+++ backend/hello_world/settings/base.py
@@ -13,19 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a@8q6l90&8#4-%+ogakse*_b*l9#)t^mx+l&d6njvx8&o4$grr'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["backend"]
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 
 
 # Application definition
---
+++ backend/hello_world/settings/dev.py
@@ -0,0 +1,12 @@
+from hello_world.settings.base import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'a@8q6l90&8#4-%+ogakse*_b*l9#)t^mx+l&d6njvx8&o4$grr'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["backend"]
